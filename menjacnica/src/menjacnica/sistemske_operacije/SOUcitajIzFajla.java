77e99c471e6ff201c9b947a7a334fdb0bf2e8d45
Petar Lakcevic <petar.lakcevic@gmail.com>
2017-05-21 19:54:29 +0200

    Izmenjena klasa menjacnica i uvedene sistemske operacije kao sto je zahtevano

---
+++ menjacnica/src/menjacnica/sistemske_operacije/SOUcitajIzFajla.java
@@ -0,0 +1,21 @@
+package menjacnica.sistemske_operacije;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
+
+public class SOUcitajIzFajla {
+	public static LinkedList<Valuta> izvrsi(String putanja) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(putanja)));
+			LinkedList<Valuta> lista = (LinkedList<Valuta>) (in.readObject());
+			in.close();
+			return lista;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
